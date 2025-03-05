fdc977e20d5190ba702f2c1aea69bc2f3ec62649
James Graham <james@hoppipolla.co.uk>
2025-03-05 20:12:48 +0000

    Modify update script

--- ci/update.sh
+++ ci/update.sh
@@ -5,11 +5,20 @@ REL_DIR_NAME=$(dirname "$0")
 SCRIPT_DIR=$(cd "$REL_DIR_NAME" && pwd -P)
 cd "$SCRIPT_DIR"/..
 
-git config --global user.email "wpt-results@mozilla.bugs"
-git config --global user.name "wpt-results-bot"
+REPO_DIR="$PWD/wpt-results"
+
+if [[ ! -e $REPO_DIR ]]; then
+    mkdir $REPO_DIR
+    git init $REPO_DIR
+fi
+
+cd $REPO_DIR
+git config user.email "wpt-results@mozilla.bugs"
+git config user.name "wpt-results-bot"
+cd ..
 
 uv run wpt-store-results --git-repo wpt-results --log-level debug --backfill=new
-cd wpt-results
+
+cd $REPO_DIR
 git status
-git log
-git push https://x-access-token:${GITHUB_TOKEN}@github.com/jgraham/gecko-wpt-results.git HEAD:main
+git push https://x-access-token:${GITHUB_TOKEN}@github.com/jgraham/gecko-results-cache.git HEAD:main
