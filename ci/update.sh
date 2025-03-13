b5ebd1fe61d267b63b5b9006feb5f2bfbc9f387d
James Graham <james@hoppipolla.co.uk>
2025-03-13 17:14:37 +0000

    Fixup fetch from remote

--- ci/update.sh
+++ ci/update.sh
@@ -6,15 +6,16 @@ SCRIPT_DIR=$(cd "$REL_DIR_NAME" && pwd -P)
 cd "$SCRIPT_DIR"/..
 
 REPO_DIR="$PWD/wpt-results"
+REMOTE_REPO="https://github.com/jgraham/gecko-results-cache.git"
 
 if [[ ! -e $REPO_DIR ]]; then
-    git clone --bare https://github.com/jgraham/gecko-results-cache.git $REPO_DIR
+    git clone --bare "$REMOTE_REPO" "$REPO_DIR"
 fi
 
 cd $REPO_DIR
 git config user.email "wpt-results@mozilla.bugs"
 git config user.name "wpt-results-bot"
-git fetch '+refs/runs/*:refs/runs/*'
+git fetch "$REMOTE_REPO" '+refs/runs/*:refs/runs/*'
 cd ..
 
 uv run wpt-store-results --git-repo wpt-results --log-level debug --backfill=new
